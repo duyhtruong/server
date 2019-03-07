8490628c27a03800a6e2302e418367e939b90bf0
Duy Truong <duytruongh@gmail.com>
2019-03-07 13:50:34 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//importing express
+const express = require('express');
+
+const app = express();
+
+
+//route handler
+app.get('/',(req, res) =>{
+	res.send({hi: 'there'});
+});
+
+app.listen(5000);
