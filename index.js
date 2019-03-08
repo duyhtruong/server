c6b8ccb0f672a5b2d1fb5402bd745143de7cb1b1
Duy Truong <duytruongh@gmail.com>
2019-03-08 14:45:28 -0800

    passport

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 //importing express
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
 
 const app = express();
 
 
+//passport use this new strategy
+passport.use(new GoogleStrategy({
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback',
+	userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+}, (accessToken, refreshToken, profile, done) => {
+	console.log('access token', accessToken);
+	console.log('refresh token', refreshToken);
+	console.log('profile:', profile);
+	})
+);
+
 //route handler
-app.get('/',(req, res) =>{
-	res.send({hi: 'there'});
-});
+app.get(
+	'/auth/google', 
+	passport.authenticate('google', {
+	scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 //heroku port process.env from heroku
 const PORT = process.env.PORT || 5000;
